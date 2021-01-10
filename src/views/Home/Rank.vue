5dd87d11f0fdf8fd35efe641098cc7fa7abc17d4
fangqiuyun <freequestionss@163.com>
2021-01-10 21:47:04 +0800

    rank

--- src/views/Home/Rank.vue
+++ src/views/Home/Rank.vue
@@ -4,3 +4,33 @@
     </div>
 
 </template>
+
+<script>
+import axios from 'axios'
+import Vue from 'vue'
+//导入uri
+import uri from "@/config/uri"
+
+Vue.use(uri)
+export default {
+    data() {
+        return {
+            //排行歌单数据源
+            list: []
+        }
+    },
+    methods: {
+        getData() {
+            // axios.get(uri.getRank + "?showDetail=1")
+            axios.get('/api/top/category?showDetail=1')
+            .then(ret => {
+                console.log(ret)
+            }).catch(err => console.log(err))
+
+        },
+    },
+    created() {
+        this.getData()
+    },
+}
+</script>
