8063015c3984938f5575c7af1623a780848574c0
liujinshuo <ljs15695432681@163.com>
2021-01-11 20:30:16 +0800

    search

--- src/views/Center/Register.vue
+++ src/views/Center/Register.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
-        <h1>注册</h1>
+        <div class="logo">
+            <img src="https://2url.cc/1OwrB" />
+        </div>
+        <div>
+            <van-form @submit="onSubmit">
+                <van-field
+                    v-model="mobile"
+                    name="mobile"
+                    label="手机号"
+                    placeholder="手机号"
+                    :rules="[{ pattern, message: '请填写正确的手机号' }]"
+                />
+                <van-field
+                    v-model="password"
+                    type="password"
+                    name="password"
+                    label="密码"
+                    placeholder="密码"
+                    :rules="[{ required: true, message: '请填写密码' }]"
+                />
+                <div style="margin: 16px;">
+                    <van-button square block type="info" native-type="submit"
+                        >提交</van-button
+                    >
+                </div>
+            </van-form>
+        </div>
     </div>
 </template>
